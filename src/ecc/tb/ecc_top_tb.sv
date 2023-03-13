cc1d1b9ab6f4bca9c005af1fa03561f1ff2858e8
Michael Norris <michnorris@microsoft.com>
2023-03-13 18:41:47 +0000

    Merged PR 101137: Adding pcr vault ports to TBs
    PCR Vault added ports to SHA512 and ECC that need to get implemented in TB.
    
    Nightly directed regression fails build when ports don't exist. It's a warning when running pb fe build, so this often gets missed before PR
    
    Related work items: #451332

--- src/ecc/tb/ecc_top_tb.sv
+++ src/ecc/tb/ecc_top_tb.sv
@@ -153,6 +153,7 @@ module ecc_top_tb
   kv_write_t kv_write_tb;
   kv_rd_resp_t [2:0] kv_rd_resp_tb;
   kv_wr_resp_t kv_wr_resp_tb;
+  pcr_signing_t pcr_signing_data_tb;
 
   wire error_intr_tb;
   wire notif_intr_tb;
@@ -206,6 +207,7 @@ module ecc_top_tb
              .kv_write(kv_write_tb),
              .kv_rd_resp(kv_rd_resp_tb),
              .kv_wr_resp(kv_wr_resp_tb),
+             .pcr_signing_data(pcr_signing_data_tb),
 
              .error_intr(error_intr_tb),
              .notif_intr(notif_intr_tb)
@@ -311,6 +313,7 @@ module ecc_top_tb
 
       kv_rd_resp_tb   = '0;
       kv_wr_resp_tb   = '0;
+      pcr_signing_data_tb = '0;
     end
   endtask // init_dut
 
