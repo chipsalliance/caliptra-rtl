bebf2f32ce44e890963f024986e28a4abbc6ab75
Kiran Upadhyayula <kupadhyayula@microsoft.com>
2023-06-07 18:54:18 +0000

    Merged PR 110104: WDT+CG fixes and coverage
    Addresses clk gating bugs
    TODO:
    - Debug cg smoke test in Verilator
    - Enable cg smoke tests in L0 regression
    
    Related work items: #464115, #470638, #477491, #477702

--- src/integration/test_suites/smoke_test_cg_wdt/caliptra_isr.h
+++ src/integration/test_suites/smoke_test_cg_wdt/caliptra_isr.h
@@ -32,34 +32,7 @@
 #include <stdint.h>
 #include "printf.h"
 
-//extern volatile uint32_t hmac_intr_status;
-
-/* --------------- symbols/typedefs --------------- */
-typedef struct {
-    uint32_t doe_error;
-    uint32_t doe_notif;
-    uint32_t ecc_error;
-    uint32_t ecc_notif;
-    uint32_t hmac_error;
-    uint32_t hmac_notif;
-    uint32_t kv_error;
-    uint32_t kv_notif;
-    uint32_t sha512_error;
-    uint32_t sha512_notif;
-    uint32_t sha256_error;
-    uint32_t sha256_notif;
-    uint32_t qspi_error;
-    uint32_t qspi_notif;
-    uint32_t uart_error;
-    uint32_t uart_notif;
-    uint32_t i3c_error;
-    uint32_t i3c_notif;
-    uint32_t soc_ifc_error;
-    uint32_t soc_ifc_notif;
-    uint32_t sha512_acc_error;
-    uint32_t sha512_acc_notif;
-} caliptra_intr_received_s;
-extern volatile caliptra_intr_received_s cptra_intr_rcv;
+extern volatile uint32_t hmac_intr_status;
 
 //////////////////////////////////////////////////////////////////////////////
 // Function Declarations
@@ -74,7 +47,20 @@ inline void service_doe_notif_intr() {printf("ERROR");}
 inline void service_ecc_error_intr   () {printf("ERROR");}
 inline void service_ecc_notif_intr   () {printf("ERROR");}
 inline void service_hmac_error_intr  () {printf("ERROR");}
-inline void service_hmac_notif_intr  () {printf("ERROR");}
+inline void service_hmac_notif_intr  () {
+    uint32_t * reg = (uint32_t *) (CLP_HMAC_REG_BASE_ADDR + HMAC_REG_INTR_BLOCK_RF_NOTIF_INTERNAL_INTR_R);
+    uint32_t sts = *reg;
+    /* Write 1 to Clear the pending interrupt */
+    if (sts & HMAC_REG_INTR_BLOCK_RF_NOTIF_INTERNAL_INTR_R_NOTIF_CMD_DONE_STS_MASK) {
+        *reg = HMAC_REG_INTR_BLOCK_RF_NOTIF_INTERNAL_INTR_R_NOTIF_CMD_DONE_STS_MASK;
+    }
+    if (sts == 0) {
+        printf("bad hmac_notif_intr sts:%x\n", sts);
+    } else {
+        reg = (uint32_t *) (CLP_HMAC_REG_HMAC384_STATUS);
+        hmac_intr_status = *reg;
+    }
+}
 
 inline void service_kv_error_intr    () {printf("ERROR");}
 inline void service_kv_notif_intr    () {printf("ERROR");}
@@ -100,7 +86,14 @@ inline void service_soc_ifc_error_intr  () {
         *reg = SOC_IFC_REG_INTR_BLOCK_RF_ERROR_INTERNAL_INTR_R_ERROR_WDT_TIMER2_TIMEOUT_STS_MASK;
     }
 }
-inline void service_soc_ifc_notif_intr  () {printf("ERROR");}
+inline void service_soc_ifc_notif_intr  () {
+    uint32_t * reg = (uint32_t *) (CLP_SOC_IFC_REG_INTR_BLOCK_RF_NOTIF_INTERNAL_INTR_R);
+    uint32_t sts = *reg;
+
+    if(sts & SOC_IFC_REG_INTR_BLOCK_RF_NOTIF_INTERNAL_INTR_R_NOTIF_DEBUG_LOCKED_STS_MASK) {
+        *reg = SOC_IFC_REG_INTR_BLOCK_RF_NOTIF_INTERNAL_INTR_R_NOTIF_DEBUG_LOCKED_STS_MASK;
+    }
+}
 inline void service_sha512_acc_error_intr() {printf("ERROR");}
 inline void service_sha512_acc_notif_intr() {printf("ERROR");}
 
