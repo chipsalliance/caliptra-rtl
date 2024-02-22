41e0fd664c256604517193870c64a9811ee23ee2
Michael Norris <michnorris@fe719.svceng.com>
2024-02-22 13:31:52 -0800

    fixing assert macros to match code from caliptra_prim difference in disable polarity caused all caliptra_prim macros that were using caliptra_sva macros to have the wrong disable polarity

--- src/libs/rtl/caliptra_sva.svh
+++ src/libs/rtl/caliptra_sva.svh
@@ -32,29 +32,29 @@
 `endif
 
 // Assert a concurrent property directly.
-`define CALIPTRA_ASSERT(assert_name, prop, clk = `CALIPTRA_ASSERT_DEFAULT_CLK, rst_b = `CALIPTRA_ASSERT_DEFAULT_RST)  \
+`define CALIPTRA_ASSERT(assert_name, prop, clk = `CALIPTRA_ASSERT_DEFAULT_CLK, rst = `CALIPTRA_ASSERT_DEFAULT_RST)  \
 `ifdef CLP_ASSERT_ON                                                           \
-  assert_name: assert property (@(posedge clk) disable iff (~rst_b) (prop))    \
+  assert_name: assert property (@(posedge clk) disable iff (rst !== 0) (prop))    \
     else begin                                                                 \
         `CALIPTRA_ASSERT_RPT(`STRINGIFY(assert_name))                                   \
     end                                                                        \
 `endif
 
 // Assert a concurrent property NEVER happens
-`define CALIPTRA_ASSERT_NEVER(assert_name, prop, clk = `CALIPTRA_ASSERT_DEFAULT_CLK, rst_b = `CALIPTRA_ASSERT_DEFAULT_RST) \
+`define CALIPTRA_ASSERT_NEVER(assert_name, prop, clk = `CALIPTRA_ASSERT_DEFAULT_CLK, rst = `CALIPTRA_ASSERT_DEFAULT_RST) \
 `ifdef CLP_ASSERT_ON                                                            \
-  assert_name: assert property (@(posedge clk) disable iff (~rst_b) not (prop)) \
+  assert_name: assert property (@(posedge clk) disable iff (rst !== 0) not (prop)) \
     else begin                                                                  \
         `CALIPTRA_ASSERT_RPT(`STRINGIFY(assert_name))                                    \
     end                                                                         \
 `endif
 
 // Assert that signal is not x
-`define CALIPTRA_ASSERT_KNOWN(assert_name, sig, clk = `CALIPTRA_ASSERT_DEFAULT_CLK, rst_b = `CALIPTRA_ASSERT_DEFAULT_RST) \
-  `CALIPTRA_ASSERT(assert_name, !$isunknown(sig), clk, rst_b)
+`define CALIPTRA_ASSERT_KNOWN(assert_name, sig, clk = `CALIPTRA_ASSERT_DEFAULT_CLK, rst = `CALIPTRA_ASSERT_DEFAULT_RST) \
+  `CALIPTRA_ASSERT(assert_name, !$isunknown(sig), clk, rst)
 
 // Assert that a vector of signals is mutually exclusive
-`define CALIPTRA_ASSERT_MUTEX(assert_name, sig, clk = `CALIPTRA_ASSERT_DEFAULT_CLK, rst_b = `CALIPTRA_ASSERT_DEFAULT_RST) \
-    `CALIPTRA_ASSERT(assert_name, $onehot0(sig), clk, rst_b)
+`define CALIPTRA_ASSERT_MUTEX(assert_name, sig, clk = `CALIPTRA_ASSERT_DEFAULT_CLK, rst = `CALIPTRA_ASSERT_DEFAULT_RST) \
+    `CALIPTRA_ASSERT(assert_name, $onehot0(sig), clk, rst)
 
 `endif
