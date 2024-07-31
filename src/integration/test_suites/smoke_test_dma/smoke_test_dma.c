886c6f269d465cab1a43eb2e8ffd56914df8dd70
Caleb Whitehead <cwhitehead@microsoft.com>
2024-07-30 17:52:21 -0700

    Add mailbox payload operations

--- src/integration/test_suites/smoke_test_dma/smoke_test_dma.c
+++ src/integration/test_suites/smoke_test_dma/smoke_test_dma.c
@@ -23,10 +23,6 @@
 #include "soc_ifc.h"
 
 
-//int whisperPrintf(const char* format, ...);
-//#define ee_printf whisperPrintf
-
-
 volatile char* stdout = (char *)STDOUT;
 volatile uint32_t intr_count       = 0;
 #ifdef CPT_VERBOSITY
@@ -85,7 +81,26 @@ void main(void) {
             0xad813451,
             0x67120ad3
         };
+        uint32_t mbox_send_payload[16] = {
+            0x0991c03c,
+            0x7bc14838,
+            0xb05f2c82,
+            0x7b233274,
+            0x01b7ba27,
+            0x3f24db45,
+            0xd945c472,
+            0xabac3989,
+            0x64af1d5e,
+            0xda068da4,
+            0xeb9102ab,
+            0xf796de3e,
+            0x88fc6af8,
+            0x1a169287,
+            0xc9a6e724,
+            0x667f9dd5
+        };
         uint32_t read_payload[16];
+        uint32_t mbox_read_payload[16];
 
         VPRINTF(LOW, "----------------------------------\nSmoke Test AXI DMA  !!\n----------------------------------\n");
 
@@ -104,10 +119,27 @@ void main(void) {
         VPRINTF(LOW, "Sending payload via AHB i/f\n");
         soc_ifc_axi_dma_send_ahb_payload(AXI_SRAM_BASE_ADDR, 0, send_payload, 16*4, 0);
 
+        // Send data through Mailbox to AXI_DMA, target the AXI SRAM
+        VPRINTF(LOW, "Writing payload to Mailbox via Direct Mode\n");
+        // Acquire the mailbox lock
+        if (soc_ifc_mbox_acquire_lock(1)) {
+            VPRINTF(ERROR, "Acquire mailbox lock failed\n");
+            fail = 1;
+        }
+        // Write data into mailbox using direct-mode
+        for (uint32_t dw = 0; dw < 16; dw++) {
+            lsu_write_32(MBOX_DIR_BASE_ADDR + 0x4400 + (dw << 2), mbox_send_payload[dw]);
+        }
+        lsu_write_32(CLP_MBOX_CSR_MBOX_UNLOCK, MBOX_CSR_MBOX_UNLOCK_UNLOCK_MASK);
+        VPRINTF(LOW, "Sending payload from Mailbox\n");
+        if (soc_ifc_axi_dma_send_mbox_payload(0x4400, AXI_SRAM_BASE_ADDR + 16*4, 0, 16*4, 0)) {
+            fail = 1;
+        }
+
         // Move data from one address to another in AXI SRAM
         // Use the block-size feature
         VPRINTF(LOW, "Moving payload at SRAM via axi-to-axi xfer\n");
-        soc_ifc_axi_dma_send_axi_to_axi(AXI_SRAM_BASE_ADDR, 0, AXI_SRAM_BASE_ADDR + AXI_SRAM_SIZE_BYTES/2, 0, 16*4, 16*2);
+        soc_ifc_axi_dma_send_axi_to_axi(AXI_SRAM_BASE_ADDR, 0, AXI_SRAM_BASE_ADDR + AXI_SRAM_SIZE_BYTES/2, 0, 2*16*4, 16*2);
 
         // Read data back from AXI SRAM and confirm it matches
         VPRINTF(LOW, "Reading payload via AHB i/f\n");
@@ -118,6 +150,27 @@ void main(void) {
                 VPRINTF(ERROR, "read_payload[%d] (0x%x) does not match send_payload[%d] (0x%x)\n", ii, read_payload[ii], ii, send_payload[ii]);
             }
         }
+
+        // Read data back through mailbox using direct-mode
+        VPRINTF(LOW, "Reading payload to Mailbox\n");
+        if (soc_ifc_axi_dma_read_mbox_payload(AXI_SRAM_BASE_ADDR + AXI_SRAM_SIZE_BYTES/2 + 16*4, 0x8800, 0, 16*4, 0)) {
+            fail = 1;
+        }
+        VPRINTF(LOW, "Reading payload from Mailbox via Direct Mode\n");
+        // Acquire the mailbox lock
+        if (soc_ifc_mbox_acquire_lock(1)) {
+            VPRINTF(ERROR, "Acquire mailbox lock failed\n");
+            fail = 1;
+        }
+        for (uint32_t dw = 0; dw < 16; dw++) {
+            mbox_read_payload[dw] = lsu_read_32(MBOX_DIR_BASE_ADDR + 0x8800 + (dw << 2));
+            if (mbox_read_payload[dw] != mbox_send_payload[dw]) {
+                fail = 1;
+                VPRINTF(ERROR, "mbox_read_payload[%d] (0x%x) does not match mbox_send_payload[%d] (0x%x)\n", dw, mbox_read_payload[dw], dw, mbox_send_payload[dw]);
+            }
+        }
+        lsu_write_32(CLP_MBOX_CSR_MBOX_UNLOCK, MBOX_CSR_MBOX_UNLOCK_UNLOCK_MASK);
+
 
         if (fail) {
             VPRINTF(FATAL, "smoke_test_dma failed!\n");
