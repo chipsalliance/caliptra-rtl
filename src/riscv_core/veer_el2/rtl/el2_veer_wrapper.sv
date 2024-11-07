a292a70be4d3f60a624adba74baafe6d9cfb4e51
Caleb Whitehead <cwhitehead@microsoft.com>
2024-11-06 17:39:48 -0800

    Route a core_enable signal to conditionally disable internal core TAP access

--- src/riscv_core/veer_el2/rtl/el2_veer_wrapper.sv
+++ src/riscv_core/veer_el2/rtl/el2_veer_wrapper.sv
@@ -434,6 +434,7 @@ import el2_pkg::*;
    input logic                             mbist_mode,    // to enable mbist
 
    // DMI port for uncore
+   input logic                             dmi_core_enable,
    input logic                             dmi_uncore_enable,
    output logic                            dmi_uncore_en,
    output logic                            dmi_uncore_wr_en,
@@ -905,6 +906,7 @@ import el2_pkg::*;
 
    // DMI core/uncore mux
    dmi_mux dmi_mux (
+    .core_enable        (dmi_core_enable),
     .uncore_enable      (dmi_uncore_enable),
 
     .dmi_en             (dmi_en),
