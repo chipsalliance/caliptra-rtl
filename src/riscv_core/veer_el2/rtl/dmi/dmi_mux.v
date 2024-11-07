08b6ec960104fd73c6ddce0c1b205f46b9563a98
Caleb Whitehead <cwhitehead@microsoft.com>
2024-11-07 14:16:06 -0800

    Update license headers on RV core

--- src/riscv_core/veer_el2/rtl/dmi/dmi_mux.v
+++ src/riscv_core/veer_el2/rtl/dmi/dmi_mux.v
@@ -1,3 +1,18 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2023 Antmicro <www.antmicro.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // DMI core aperture ranges from 0x00 to 0x4F. Addresses starting from 0x50
 // and above are considered uncore.
 
