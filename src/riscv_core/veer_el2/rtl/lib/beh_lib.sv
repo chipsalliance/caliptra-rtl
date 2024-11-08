16837fdc35eb0f4bc87583c5d334e0021a46ef2e
Caleb Whitehead <cwhitehead@microsoft.com>
2024-11-08 11:42:24 -0800

    Revert latch fix that causes Verilator failures

--- src/riscv_core/veer_el2/rtl/lib/beh_lib.sv
+++ src/riscv_core/veer_el2/rtl/lib/beh_lib.sv
@@ -760,10 +760,16 @@ module `TEC_RV_ICG
 
    assign      enable = EN | SE;
 
+`ifdef VERILATOR
+   always @(negedge CK) begin
+      en_ff <= enable;
+   end
+`else
    always @(CK, enable) begin
       if(!CK)
         en_ff = enable;
    end
+`endif
    assign Q = CK & en_ff;
 
 endmodule
