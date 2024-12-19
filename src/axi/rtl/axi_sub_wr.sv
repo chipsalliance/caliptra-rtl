94180918004888fafa0bfe86f762488f72a1819d
Anjana Parthasarathy <aparthas@outlook.com>
2024-12-19 14:08:43 -0800

    Fixed typo in axi_sub_wr

--- src/axi/rtl/axi_sub_wr.sv
+++ src/axi/rtl/axi_sub_wr.sv
@@ -237,7 +237,7 @@ module axi_sub_wr import axi_pkg::*; #(
     always_comb addr = {txn_ctx.addr[AW-1:BW],BW'(0)};
     always_comb user = txn_ctx.user;
     always_comb id   = txn_ctx.id;
-    always_comb size = txn_ctx.size;
+    always_comb wsize = txn_ctx.size;
 
     // Use full address to calculate next address (in case of AxSIZE < data width)
     axi_addr #(
