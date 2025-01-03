a4ff26b0237e8abc2356709eae90e8e9da14bb12
Clayton Kuchta <ckuchta@microsoft.com>
2025-01-03 14:59:14 -0800

    1. Make WDT more moduler by removing the soc_ifc_pkg import and adding parameters to the module. - Better reusability with MCI 2. Move the wdt_timer*_timeout_serviced write restrictions from soc_ifc_top -> wdt. Better reusability with MCI. 3. Simplify moduler the timer*_count logic by creating new *_qual and *_restart signals that are separate from the timer*_count logic. - Code clean up 4. HW enfore the timer2_count cannot be reset by timer2_restart unless we are in independent mode. - New functionality 5. Add new fatal_error to WDT that is equivalent to nmi_intr logic in soc_ifc_top making it more resuable for MCI WDT. - Better reusability with MCI

--- src/soc_ifc/rtl/wdt.sv
+++ src/soc_ifc/rtl/wdt.sv
@@ -13,7 +13,10 @@
 // limitations under the License.
 
 module wdt
-    import soc_ifc_pkg::*;
+    #(
+    parameter  WDT_TIMEOUT_PERIOD_NUM_DWORDS = 2,
+    localparam WDT_TIMEOUT_PERIOD_W = WDT_TIMEOUT_PERIOD_NUM_DWORDS * 32
+    )
 (
     input  logic clk,
     input  logic cptra_rst_b,
@@ -29,23 +32,53 @@ module wdt
     input  logic wdt_timer2_timeout_serviced,
     //WDT STATUS bits
     output logic t1_timeout,
-    output logic t2_timeout
+    output logic t2_timeout,
+    output logic fatal_timeout
+
 );
 
 //Timer1
 logic [WDT_TIMEOUT_PERIOD_W-1:0] timer1_count;
 logic timer1_count_en;
+logic wdt_timer1_timeout_serviced_qual;
+logic wdt_timer1_timeout_serviced_restart;
 
 //Timer2
 logic [WDT_TIMEOUT_PERIOD_W-1:0] timer2_count;
 logic timer2_count_en;
+logic wdt_timer2_timeout_serviced_qual;
+logic wdt_timer2_timeout_serviced_restart;
 
 //Output assignments
 assign t1_timeout = (timer1_count == timer1_timeout_period);
 assign t2_timeout = (timer2_count == timer2_timeout_period);
 
+
 assign timer1_count_en = timer1_en;
 assign timer2_count_en = !timer2_en ? t1_timeout : timer2_en;
+
+// In cascade mode if T2 timesout it is a fatal error.
+assign fatal_timout = t2_timeout && !timer2_en;          
+
+
+// Timeout Service Logic
+
+// Only acknowledge the T1 timeout servicing request if T1 has timeout
+assign wdt_timer1_timeout_serviced_qual = wdt_timer1_timeout_serviced & t1_timout 
+
+// Only restart T1 via timeout service request if:
+//  1. In independent mode and T1 has timed out
+//  2. In cascade mode and T2 hasn't timed out
+assign wdt_timer1_timeout_serviced_restart = wdt_timer1_timeout_serviced_qual & (timer2_en || !t2_timeout);
+
+
+// Only acknowledge the T2 timeout servicing request if T2 has timeout and in parallel mode
+assign wdt_timer2_timeout_serviced_qual = wdt_timer2_timeout_serviced & t2_timout & timer2_en; 
+
+// Only restart T2 via timeout service request if:
+//  1. In casecade mode T2 hasn't timeout out and T1 has been serviced
+//  2. In independent mode and T2 has timed out
+assign wdt_timer2_timeout_serviced_restart = (wdt_timer1_timeout_serviced_qual & !t2_timeout && !timer2_en) | wdt_timer2_timeout_serviced_qual;
 
 
 //Timer1
@@ -60,7 +93,7 @@ always_ff @(posedge clk or negedge cptra_rst_b) begin
         //start count from 0 again. In all other cases, hold the count
         //Note: if t2 has timed out, expect a warm reset to re-init timers
         timer1_count <= timer1_restart && !t1_timeout ? 'h0 
-                                                    : (wdt_timer1_timeout_serviced && (timer2_en || !t2_timeout)) ? 'h0 
+                                                    : wdt_timer1_timeout_serviced_restart ? 'h0 
                                                     : !t1_timeout ? (timer1_count + 'h1)
                                                     : timer1_count;
     end
@@ -77,10 +110,11 @@ always_ff @(posedge clk or negedge cptra_rst_b) begin
         //hold the count. In case of time out, nmi and fatal error are triggered
         //Warm reset is expected to re-init timers
         //Note: if timer1 and timer2 are independently enabled, we want to remove the dependency of timer1 on timer2 and vice versa
-        timer2_count <= timer2_restart && !t2_timeout ? 'h0 
-                                                    : ((wdt_timer1_timeout_serviced && !t2_timeout && !timer2_en) || wdt_timer2_timeout_serviced) ? 'h0 
-                                                    : !t2_timeout ? (timer2_count + 'h1) 
-                                                    : timer2_count;
+        //Note: Only allow timer2_restart to reset T2 if in independent mode 
+        timer2_count <= timer2_restart && timer2_en && !t2_timeout ? 'h0 
+                                                                    : wdt_timer2_timeout_serviced_restart ? 'h0 
+                                                                    : !t2_timeout ? (timer2_count + 'h1) 
+                                                                    : timer2_count;
     end
 end
 
