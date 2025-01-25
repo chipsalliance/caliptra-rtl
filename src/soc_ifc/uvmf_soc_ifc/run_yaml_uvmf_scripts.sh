f7897bf851c8bbd98aeed04c5a48bbd2672abf90
Caleb Whitehead <cwhitehead@microsoft.com>
2025-01-24 17:24:07 -0800

    Update yaml UVMF generators with new signals in soc_ifc; add dummy AVERY AAXI placeholder (so generated code uses correct initialize indices, agent count, etc)

--- src/soc_ifc/uvmf_soc_ifc/run_yaml_uvmf_scripts.sh
+++ src/soc_ifc/uvmf_soc_ifc/run_yaml_uvmf_scripts.sh
@@ -9,8 +9,8 @@ python ${UVMF_HOME}/scripts/yaml2uvmf.py --merge_source uvmf_template_output \
                                          mbox_sram_interface.yaml \
                                          soc_ifc_util_comp_soc_ifc_predictor.yaml \
                                          soc_ifc_util_comp_soc_ifc_scoreboard.yaml \
+                                         avery_aaxi_interface.yaml \
                                          ../../libs/uvmf/qvip_ahb_lite_slave_dir/uvmf/qvip_ahb_lite_slave_subenv_config.yaml \
-                                         ../../libs/uvmf/qvip_apb5_slave_dir/uvmf/qvip_apb5_slave_subenv_config.yaml \
                                          soc_ifc_environment.yaml \
                                          soc_ifc_bench.yaml \
                                          -d uvmf_template_output_merged
