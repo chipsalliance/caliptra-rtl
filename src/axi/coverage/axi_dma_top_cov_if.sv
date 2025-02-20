8190778bb02751c143dd9ad15cac441a26fda1f2
Anjana Parthasarathy <aparthas@outlook.com>
2025-02-19 16:07:12 -0800

    Enabled functional coverage for AXI DMA and verified AHB2AXI_1DW coverpoint is hit

--- src/axi/coverage/axi_dma_top_cov_if.sv
+++ src/axi/coverage/axi_dma_top_cov_if.sv
@@ -36,30 +36,50 @@ interface axi_dma_top_cov_if
         // Component INF
         input logic dv,
         input var soc_ifc_req_t req_data,
-        output logic hold,
-        output logic [SOC_IFC_DATA_W-1:0] rdata,
-        output logic error,
 
         // Mailbox SRAM INF
-        output logic                   mb_dv,
+
         input  logic                   mb_hold,
         input  logic                   mb_error,
-        output var soc_ifc_req_t       mb_data,
         input  logic [DW-1:0]          mb_rdata
     );
+
+    logic w_valid;
+
+    assign w_valid = axi_dma_top.w_valid;
 
     // Queue to store write data input Component INF
     logic [SOC_IFC_DATA_W-1:0] req_data_queue[$];
 
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            req_data_queue.clear();
-        end else if (dv && req_data.write) begin
+            req_data_queue = {};
+        end else if (w_valid && dv && req_data.write) begin
             req_data_queue.push_back(req_data.wdata);
         end
     end
 
-    covergroup axi_dma_cov_grp @(posedge clk);
+    // Capture expected wdata after awvalid has been asserted. 
+    logic awvalid_seen;
+    logic [SOC_IFC_DATA_W-1:0] expected_wdata;
+
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            awvalid_seen <= 0;
+            expected_wdata <= 0;
+        end 
+        else if (m_axi_w_if.awvalid && m_axi_w_if.awready && m_axi_w_if.awsize == 3'b010) begin
+            awvalid_seen <= 1;
+        end
+        else if (awvalid_seen && m_axi_w_if.wvalid) begin
+                expected_wdata <= req_data_queue.pop_front();
+        end
+        else if (!m_axi_w_if.wvalid) begin
+            awvalid_seen <= 0;
+        end
+    end
+
+    covergroup axi_dma_top_cov_grp @(posedge clk);
         option.per_instance = 1;
 
         //-------------------------------------------------------------
@@ -67,12 +87,12 @@ interface axi_dma_top_cov_if
         //-------------------------------------------------------------
 
         // AHB2AXI_1DW Transfer with Data Comparison
-        ahb2axi_1dw_data_compare: coverpoint (m_axi_r_if.awvalid && m_axi_w_if.awready && m_axi_r_if.awsize == 3'b010 && 
-                                                m_axi_w_if.wdata == req_data_queue.pop_front()) {
+        ahb2axi_1dw_data_compare: coverpoint (awvalid_seen && m_axi_w_if.wvalid && m_axi_w_if.wdata == expected_wdata) {
             bins single_4byte_wr_match = {1}; // Cover 4-byte write transfers with matching data
         }
-        
     endgroup
+
+    axi_dma_top_cov_grp axi_dma_top_cov_grp1 = new();
 endinterface
 
 `endif
