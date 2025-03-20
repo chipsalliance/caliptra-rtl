2ed4d58d46567b9def7e0c1b597fd739d565ee53
Mojtaba Bisheh Niasar <mojtabab@microsoft.com>
2025-03-20 14:57:46 -0700

    added failure injection test

--- src/hmac_drbg/rtl/hmac_drbg.sv
+++ src/hmac_drbg/rtl/hmac_drbg.sv
@@ -135,6 +135,8 @@ module hmac_drbg
   logic [REG_SIZE-1:0]  HMAC_tag;
   logic [511:0]         HMAC512_tag;
 
+  logic                 failure_check;
+
   //----------------------------------------------------------------
   // HMAC module instantiation.
   //----------------------------------------------------------------
@@ -359,7 +361,7 @@ module hmac_drbg
 
       CHCK_ST:
       begin
-        if ((HMAC_tag==0) || (HMAC_tag >= HMAC_DRBG_PRIME))
+        if (failure_check)
           drbg_next_st    = K3_ST;
         else
           drbg_next_st    = DONE_ST;
@@ -372,6 +374,8 @@ module hmac_drbg
 
     endcase
   end //state_logic
+
+  always_comb failure_check = (HMAC_tag==0) || (HMAC_tag >= HMAC_DRBG_PRIME);
 
   //----------------------------------------------------------------
   // Concurrent connectivity for ports etc.
