7df23b42b32c3bc3c77a39ddf4ec19b331f47dd6
Caleb <11879229+calebofearth@users.noreply.github.com>
2025-06-10 11:46:23 -0700

    [VAL] Add invalid command parsing error injection scenarios for DMA test (#895)
    * Add invalid command parsing error injection scenarios
    
    * MICROSOFT AUTOMATED PIPELINE: Stamp 'cwhitehead-msft-dma-err-inj-case' with updated timestamp and hash after successful run

--- src/integration/test_suites/smoke_test_dma/smoke_test_dma.c
+++ src/integration/test_suites/smoke_test_dma/smoke_test_dma.c
@@ -49,8 +49,96 @@ const enum tb_fifo_mode {
     RAND_DELAY_TOGGLE   = 0x8f
 };
 
+enum err_inj_type {
+//    cmd_inv_rd_route    ,
+//    cmd_inv_wr_route    ,
+    cmd_inv_route_combo ,
+    cmd_inv_src_addr    ,
+    cmd_inv_dst_addr    ,
+    cmd_inv_byte_count  ,
+    cmd_inv_block_size  ,
+    cmd_inv_rd_fixed    ,
+    cmd_inv_wr_fixed    ,
+    cmd_inv_mbox_lock
+//    cmd_inv_sha_lock
+};
+
 void nmi_handler() {
     VPRINTF(FATAL, "NMI");
+}
+uint8_t soc_ifc_axi_dma_inject_inv_error(enum err_inj_type err_type) {
+    uint32_t reg;
+    uint64_t src_addr;
+    uint64_t dst_addr;
+    uint32_t rd_route;
+    uint32_t wr_route;
+    uint8_t  rd_fixed;
+    uint8_t  wr_fixed;
+    uint32_t byte_count;
+    uint16_t block_size;
+
+    src_addr   = AXI_SRAM_BASE_ADDR + ((err_type == cmd_inv_src_addr) ? 0x3 : 0x0);
+    dst_addr   = (err_type == cmd_inv_mbox_lock) ? 0x4000 : (AXI_SRAM_BASE_ADDR + 0x4000 + ((err_type == cmd_inv_dst_addr) ? 0x3 : 0x0));
+    rd_route   = (err_type == cmd_inv_rd_fixed) ? axi_dma_rd_route_DISABLE : (err_type == cmd_inv_route_combo) ? axi_dma_rd_route_AHB_FIFO : (err_type == cmd_inv_mbox_lock) ? axi_dma_rd_route_MBOX    : axi_dma_rd_route_AXI_WR;
+    wr_route   = (err_type == cmd_inv_wr_fixed) ? axi_dma_wr_route_DISABLE : (err_type == cmd_inv_route_combo) ? axi_dma_wr_route_AHB_FIFO : (err_type == cmd_inv_mbox_lock) ? axi_dma_wr_route_DISABLE : axi_dma_wr_route_AXI_RD;
+    rd_fixed   = err_type == cmd_inv_rd_fixed;
+    wr_fixed   = err_type == cmd_inv_wr_fixed;
+    byte_count = (err_type == cmd_inv_byte_count) ? 0x43 : 0x40;
+    block_size = (err_type == cmd_inv_block_size) ? 0x13 : 0x00;
+
+    VPRINTF(HIGH, "param: src_addr   0x%x 0x%x\n", (uint32_t) (src_addr >> 32) , (uint32_t) (src_addr & 0xffffffff));
+    VPRINTF(HIGH, "param: dst_addr   0x%x 0x%x\n", (uint32_t) (dst_addr >> 32) , (uint32_t) (dst_addr & 0xffffffff));
+    VPRINTF(HIGH, "param: rd_route   0x%x\n"     , rd_route  );
+    VPRINTF(HIGH, "param: wr_route   0x%x\n"     , wr_route  );
+    VPRINTF(HIGH, "param: rd_fixed   0x%x\n"     , rd_fixed  );
+    VPRINTF(HIGH, "param: wr_fixed   0x%x\n"     , wr_fixed  );
+    VPRINTF(HIGH, "param: byte_count 0x%x\n"     , byte_count);
+    VPRINTF(HIGH, "param: block_size 0x%x\n"     , block_size);
+
+    // Disable txn_done interrupt since we'll poll it
+    reg = lsu_read_32(CLP_AXI_DMA_REG_INTR_BLOCK_RF_NOTIF_INTR_EN_R);
+    lsu_write_32(CLP_AXI_DMA_REG_INTR_BLOCK_RF_NOTIF_INTR_EN_R, reg & ~AXI_DMA_REG_INTR_BLOCK_RF_NOTIF_INTR_EN_R_NOTIF_TXN_DONE_EN_MASK);
+
+    // Arm the Error command
+    while (lsu_read_32(CLP_AXI_DMA_REG_STATUS0) & AXI_DMA_REG_STATUS0_BUSY_MASK);
+    VPRINTF(LOW, "FW: Arm err command\n");
+    lsu_write_32(CLP_AXI_DMA_REG_SRC_ADDR_L,  src_addr        & 0xffffffff);
+    lsu_write_32(CLP_AXI_DMA_REG_SRC_ADDR_H, (src_addr >> 32) & 0xffffffff);
+    lsu_write_32(CLP_AXI_DMA_REG_DST_ADDR_L,  dst_addr        & 0xffffffff);
+    lsu_write_32(CLP_AXI_DMA_REG_DST_ADDR_H, (dst_addr >> 32) & 0xffffffff);
+    lsu_write_32(CLP_AXI_DMA_REG_BYTE_COUNT, byte_count);
+    lsu_write_32(CLP_AXI_DMA_REG_BLOCK_SIZE, (uint32_t) block_size);
+    reg = (AXI_DMA_REG_CTRL_GO_MASK)                      |
+          (rd_route << AXI_DMA_REG_CTRL_RD_ROUTE_LOW)     |
+          (wr_route << AXI_DMA_REG_CTRL_WR_ROUTE_LOW)     |
+          (rd_fixed ? AXI_DMA_REG_CTRL_RD_FIXED_MASK : 0) |
+          (wr_fixed ? AXI_DMA_REG_CTRL_WR_FIXED_MASK : 0);
+    lsu_write_32(CLP_AXI_DMA_REG_CTRL, reg);
+
+    // Check completion (error state expected)
+    reg = lsu_read_32(CLP_AXI_DMA_REG_STATUS0);
+    VPRINTF(LOW, "FW: Poll DMA status\n");
+    while ((reg & AXI_DMA_REG_STATUS0_BUSY_MASK) && !(reg & AXI_DMA_REG_STATUS0_ERROR_MASK)) {
+        reg = lsu_read_32(CLP_AXI_DMA_REG_STATUS0);
+    }
+
+    // Check status
+    VPRINTF(LOW, "FW: Check DMA status\n");
+    if (reg & AXI_DMA_REG_STATUS0_ERROR_MASK) {
+        VPRINTF(LOW, "AXI DMA reports err status for err injection xfer\n");
+        lsu_write_32(CLP_AXI_DMA_REG_CTRL, AXI_DMA_REG_CTRL_FLUSH_MASK);
+    } else {
+        VPRINTF(ERROR, "ERROR: AXI DMA reports success status for err injection xfer!\n");
+        SEND_STDOUT_CTRL(0x1);
+        while(1);
+        //return 1;
+    }
+
+    // Reenable txn_done interrupt
+    reg = lsu_read_32(CLP_AXI_DMA_REG_INTR_BLOCK_RF_NOTIF_INTR_EN_R);
+    lsu_write_32(CLP_AXI_DMA_REG_INTR_BLOCK_RF_NOTIF_INTR_EN_R, reg | AXI_DMA_REG_INTR_BLOCK_RF_NOTIF_INTR_EN_R_NOTIF_TXN_DONE_EN_MASK);
+
+    return 0;
 }
 void main(void) {
         int argc=0;
@@ -148,7 +236,14 @@ void main(void) {
                                                                             AXI_DMA_REG_INTR_BLOCK_RF_NOTIF_INTR_EN_R_NOTIF_FIFO_NOT_FULL_EN_MASK));
 
         // Test each malformed command check
-        // TODO
+        if (soc_ifc_axi_dma_inject_inv_error(cmd_inv_route_combo)) { fail = 1; }
+        if (soc_ifc_axi_dma_inject_inv_error(cmd_inv_src_addr   )) { fail = 1; }
+        if (soc_ifc_axi_dma_inject_inv_error(cmd_inv_dst_addr   )) { fail = 1; }
+        if (soc_ifc_axi_dma_inject_inv_error(cmd_inv_byte_count )) { fail = 1; }
+        if (soc_ifc_axi_dma_inject_inv_error(cmd_inv_block_size )) { fail = 1; }
+        if (soc_ifc_axi_dma_inject_inv_error(cmd_inv_rd_fixed   )) { fail = 1; }
+        if (soc_ifc_axi_dma_inject_inv_error(cmd_inv_wr_fixed   )) { fail = 1; }
+        if (soc_ifc_axi_dma_inject_inv_error(cmd_inv_mbox_lock  )) { fail = 1; }
 
         // ===========================================================================
         // If reset was executed, try to run another simple DMA test to check for life
