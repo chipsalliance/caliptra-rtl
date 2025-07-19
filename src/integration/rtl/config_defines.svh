b1ad20d6a0e9e24f0dc9838c82fffe5a3213a590
Caleb Whitehead <cwhitehead@microsoft.com>
2025-07-18 23:06:02 -0700

    Enable OCP_LOCK via compile macro

--- src/integration/rtl/config_defines.svh
+++ src/integration/rtl/config_defines.svh
@@ -20,7 +20,7 @@
 // Uncomment to enable Caliptra Internal TRNG
 //`define CALIPTRA_INTERNAL_TRNG
 // Uncomment to enable OCP LOCK features
-//`define CALIPTRA_OCP_LOCK_EN
+`define CALIPTRA_OCP_LOCK_EN
 
 // Uncomment if fuse granularity 32 bits.
 //`define CALIPTRA_FUSE_GRANULARITY_32
