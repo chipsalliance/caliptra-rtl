fe672ec1793ef7db22c7cf088ff4b744956c9fda
Caleb Whitehead <cwhitehead@microsoft.com>
2025-08-11 16:53:05 -0700

    Move ocp_lock_en strap set to bfm

--- src/integration/tb/caliptra_top_tb_soc_bfm.sv
+++ src/integration/tb/caliptra_top_tb_soc_bfm.sv
@@ -46,6 +46,8 @@ import caliptra_top_tb_pkg::*; #(
     output logic [15:0] strap_ss_key_release_key_size,
     output logic [63:0] strap_ss_key_release_base_addr,
 
+    output logic ss_ocp_lock_en,
+
     input logic ready_for_fuses,
     input logic ready_for_mb_processing,
     input logic mailbox_data_avail,
@@ -172,6 +174,13 @@ import caliptra_top_tb_pkg::*; #(
             $display("STRAP_SS_KEY_RELEASE_KEY_SIZE set to default value 0x%04x", strap_ss_key_release_key_size);
         end
         
+        if ($test$plusargs("CLP_OCP_LOCK_EN")) begin
+            ss_ocp_lock_en = 1'b1;
+        end
+        else begin
+            ss_ocp_lock_en = 1'b0;
+        end
+
         // Initialize strap_ss_key_release_base_addr based on plusargs
         if ($test$plusargs("STRAP_SS_KEY_RELEASE_BASE_ADDR_RAND_SRAM")) begin
             logic [63:0] random_offset;
