4410ba37d870fd4dffa92a22b56fe22b4e0dd0a8
Clayton Kuchta <ckuchta@microsoft.com>
2025-08-17 21:58:02 -0700

    [TB] Fix Verilator build errors

--- src/integration/tb/caliptra_top_tb_soc_bfm.sv
+++ src/integration/tb/caliptra_top_tb_soc_bfm.sv
@@ -156,7 +156,11 @@ import caliptra_top_tb_pkg::*; #(
             $display("STRAP_SS_KEY_RELEASE_KEY_SIZE randomized (0x4-0x40, DWORD aligned) to 0x%04x", strap_ss_key_release_key_size);
         end
         else if ($test$plusargs("STRAP_SS_KEY_RELEASE_KEY_SIZE_RAND_HIGH")) begin
+`ifdef CLP_ASSERT_ON
+    `ifndef VERILATOR
             $assertoff(0, `CPTRA_TOP_PATH.soc_ifc_top1.SS_STRAP_KEY_SIZE_LTE_64);
+    `endif // VERILATOR
+`endif // CLP_ASSERT_ON
             strap_ss_key_release_key_size = $urandom_range(16'h44, 16'hFFFF);
             // Ensure DWORD alignment by clearing lower 2 bits
             strap_ss_key_release_key_size = strap_ss_key_release_key_size & ~16'h3;
