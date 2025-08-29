186aebd21cf3a3716b2a68f47979666a41eb6255
clayton8 <ckuchta@microsoft.com>
2025-08-29 16:24:09 -0600

    [TB] Fix smoke_test_aes_kv_out_rand  (#1017)
    * [TB] fix smoke_test_aes_kv_out_rand where kv_reuse_key wasn't getting initialized
    
    * [TB] Weight KV16 and KV24 selection in aes KV rand test
    
    * Update smoke_test_aes_kv_out_rand.c

--- src/integration/test_suites/smoke_test_aes_kv_out_rand/smoke_test_aes_kv_out_rand.c
+++ src/integration/test_suites/smoke_test_aes_kv_out_rand/smoke_test_aes_kv_out_rand.c
@@ -92,16 +92,43 @@ void main(void) {
 
     for(int i = 0; i < 50; i++) {
         VPRINTF(LOW, "START TEST %d\n", i);
+        aes_key_o = (aes_key_o_t){0};
+        aes_key = (aes_key_t){0};
+
         aes_key_o.kv_intf = TRUE;
         aes_key_o.kv_expect_err = FALSE;
-        aes_key_o.kv_id = xorshift32() % 24; //KV slot 0-23
+        uint32_t rand_val = xorshift32() % 100;  // 0-99
+        if (rand_val < 10) {
+            // 10% chance: use ID 16
+            aes_key_o.kv_id = 16;
+        } else if(rand_val < 20) {
+            // 10% chance: use ID 23
+            aes_key_o.kv_id = 23;
+        }else {
+            // 80% chance: randomly select from remaining 22 IDs (0-15, 17-22)
+            while(aes_key_o.kv_id == 16 || aes_key_o.kv_id == 23) {
+                aes_key_o.kv_id = xorshift32() % 24 ;
+            }
+        }
         aes_key_o.dest_valid = (dest_valid_t){0}; // Clear all destinations
         aes_key_o.dest_valid.dma_data = 1; // Only allow DMA access
             
         
         aes_key.kv_intf = (xorshift32() % 2) ? TRUE : FALSE;
         if (aes_key.kv_intf == TRUE) {
-            aes_key.kv_id = xorshift32() % 24;
+            rand_val = xorshift32() % 100;  // 0-99
+            if (rand_val < 10) {
+                // 10% chance: use ID 16
+                aes_key.kv_id = 16;
+            } else if(rand_val < 20) {
+                // 10% chance: use ID 23
+                aes_key.kv_id = 23;
+            }else {
+                // 80% chance: randomly select from remaining 22 IDs (0-15, 17-22)
+                while(aes_key.kv_id == 16 || aes_key.kv_id == 23) {
+                    aes_key.kv_id = xorshift32() % 24 ;
+                }
+            }
         }
 
 
