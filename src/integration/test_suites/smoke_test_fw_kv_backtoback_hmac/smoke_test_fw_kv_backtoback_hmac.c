b46dafac669c94d2965663b73d78a44ad85fe2e2
Mojtaba Bisheh Niasar <mojtabab@microsoft.com>
2025-09-02 08:01:48 -0700

    updated hmac key injection

--- src/integration/test_suites/smoke_test_fw_kv_backtoback_hmac/smoke_test_fw_kv_backtoback_hmac.c
+++ src/integration/test_suites/smoke_test_fw_kv_backtoback_hmac/smoke_test_fw_kv_backtoback_hmac.c
@@ -162,8 +162,7 @@ void main() {
 
 
     //inject hmac384_key to kv key reg (in RTL)
-    uint8_t key_inject_cmd = 0xa0 + (hmac384_key.kv_id & 0x7);
-    SEND_STDOUT_CTRL(key_inject_cmd);
+    lsu_write_32(STDOUT, (hmac384_key.kv_id << 8) | 0xa0);
 
     hmac384_flow(hmac384_key, hmac_block, hmac_lfsr_seed, hmac384_tag, TRUE, FALSE);
     
