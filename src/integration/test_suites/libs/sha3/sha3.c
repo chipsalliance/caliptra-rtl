97f5e43ef131d0caeb81f87aafc665c32a8c1363
Marno van der Maas <34654485+marnovandermaas@users.noreply.github.com>
2025-09-22 17:01:29 +0100

    [TESTS] SHA3 external mu test (#956)
    * [TESTS] External mu test uses SHA3 block
    
    This creates a check_external_mu function inside the
    smoke_test_mldsa_external_mu test. Before invoking the external mu flow
    in Adam's bridge it now calculates the external mu with the SHA3 engine
    and compares this to the pre-computed value. If these match then it goes
    through the normal MLDSA external mu flow as before.
    
    * [TESTS] MLDSA smoke test fix vectors
    
    Before these test vectors were reversed before going through the MLDSA
    flow which adds about 10 minutes of simulation time on Verilator. This
    commit reverses the constant vectors to avoid wasting this time. The
    commit adjusts both the MLDSA test as well as the external mu variant.
    
    * [TESTS] Message endianness now configurable
    
    Also change the calls to the SHA3 start function in cshake, sha3,
    interrupt and external mu tests.
    
    * [TESTS] SHA3 external mu test added
    
    This runs ACVP NIST KAT vectors through the newly added SHA3 block. The
    test runs 12 vectors through the SHA3 block with public key and
    processed message and compares it to a pre-computed value made by
    dilithium-py.
    
    Here are the original vectors:
    https://raw.githubusercontent.com/usnistgov/ACVP-Server/refs/heads/master/gen-val/json-files/ML-DSA-sigGen-FIPS204/internalProjection.json
    
    * [TESTS] Confusing comment removed
    
    * [TB] External mu forcing services
    
    This implements 4 separate forcing sequences. These are necessary for
    full random external mu testing. Here are the codes:
    - 0xD0: force the public key into the SHA3 message FIFO.
    - 0xD1: force the message into the SHA3 message FIFO.
    - 0xD2: start keygen and signing for ABR without injecting message.
    - 0xD3: start verification without injecting message.
    
    Co-authored-by: Gary Guo <gary.guo@lowrisc.org>
    
    * [TESTS] External mu full random test
    
    This exercises the new commands added in the testbench services and is
    based on the smoke_test_mldsa_keygen_sign_vfy_rand test.
    
    Move the initialisation of the interrupts to after we've done forcing
    signals inside the SHA3 block.
    
    * [TB] Trailing whitespace removed from services
    
    * [TESTS] External mu added to random nightly
    
    * [TESTS] SHA3 interrupts added to directed tests
    
    * [TESTS] SHA3 Caliptra ISR updates
    
    For the tests using SHA3 add functionality to deal with interrupts.
    
    * Revert "[TESTS] SHA3 and CSHAKE smoke tests no notif"
    
    This reverts commit 98fe31cc8d9d27b9b91d3dcb4c9b7375e6ab9a57.
    
    * [TESTS] External mu rand test prints
    
    Add prints to help find the progression of the external mu random test.
    
    * [TB] Parameterise MLDSA test vector
    
    * MICROSOFT AUTOMATED PIPELINE: Stamp 'lowrisc_externalmu' with updated timestamp and hash after successful run
    
    ---------
    
    Co-authored-by: Gary Guo <gary.guo@lowrisc.org>
    Co-authored-by: Antonio Martinez Zambrana (LOWRISC C I C) <v-amartinezz@microsoft.com>

--- src/integration/test_suites/libs/sha3/sha3.c
+++ src/integration/test_suites/libs/sha3/sha3.c
@@ -114,7 +114,7 @@ void dif_kmac_function_name_init(const char *data, size_t len, dif_kmac_function
 
 void dif_kmac_mode_sha3_start(
     const uintptr_t kmac, dif_kmac_operation_state_t *operation_state,
-    dif_kmac_mode_sha3_t mode) {
+    dif_kmac_mode_sha3_t mode, const dif_kmac_msg_endianness_t msg_endianness) {
   if (kmac == 0 || operation_state == 0) {
     VPRINTF(ERROR, "dif_kmac_mode_sha3_start: ERROR kmac or operation_state NULL.\n");
     while(1);
@@ -168,7 +168,8 @@ void dif_kmac_mode_sha3_start(
 
   // Configure SHA-3 mode with the given strength.
   // Must be written twice because it is a shadow register.
-  uint32_t config_reg = (kstrength << KMAC_CFG_SHADOWED_KSTRENGTH_LOW) |
+  uint32_t config_reg = (msg_endianness << KMAC_CFG_SHADOWED_MSG_ENDIANNESS_LOW) |
+                        (kstrength << KMAC_CFG_SHADOWED_KSTRENGTH_LOW) |
                         (KMAC_CFG_SHADOWED_MODE_VALUE_SHA3 << KMAC_CFG_SHADOWED_MODE_LOW);
   lsu_write_32(kmac + KMAC_CFG_SHADOWED, config_reg);
   lsu_write_32(kmac + KMAC_CFG_SHADOWED, config_reg);
@@ -184,7 +185,7 @@ void dif_kmac_mode_sha3_start(
 
 void dif_kmac_mode_shake_start(
     const uintptr_t kmac, dif_kmac_operation_state_t *operation_state,
-    dif_kmac_mode_shake_t mode) {
+    dif_kmac_mode_shake_t mode, const dif_kmac_msg_endianness_t msg_endianness) {
   if (kmac == 0 || operation_state == NULL) {
     VPRINTF(ERROR, "dif_kmac_mode_shake_start: ERROR kmac and operation state cannot be NULL.\n");
     while (1);
@@ -221,8 +222,9 @@ void dif_kmac_mode_shake_start(
   operation_state->offset = 0;
 
   // Configure SHAKE mode with the given strength.
-  uint32_t cfg_reg = (kstrength << KMAC_CFG_SHADOWED_KSTRENGTH_LOW) |
-                        (KMAC_CFG_SHADOWED_MODE_VALUE_SHAKE << KMAC_CFG_SHADOWED_MODE_LOW);
+  uint32_t cfg_reg = (msg_endianness << KMAC_CFG_SHADOWED_MSG_ENDIANNESS_LOW) |
+                     (kstrength << KMAC_CFG_SHADOWED_KSTRENGTH_LOW) |
+                     (KMAC_CFG_SHADOWED_MODE_VALUE_SHAKE << KMAC_CFG_SHADOWED_MODE_LOW);
   lsu_write_32(kmac + KMAC_CFG_SHADOWED, cfg_reg);
   lsu_write_32(kmac + KMAC_CFG_SHADOWED, cfg_reg);
 
@@ -235,7 +237,7 @@ void dif_kmac_mode_shake_start(
 void dif_kmac_mode_cshake_start(
     const uintptr_t kmac, dif_kmac_operation_state_t *operation_state,
     dif_kmac_mode_cshake_t mode, const dif_kmac_function_name_t *n,
-    const dif_kmac_customization_string_t *s) {
+    const dif_kmac_customization_string_t *s, const dif_kmac_msg_endianness_t msg_endianness) {
   if (kmac == 0 || operation_state == NULL) {
     VPRINTF(ERROR, "dif_kmac_mode_cshake_start: ERROR kmac or operation state is NULL.\n");
     while (1);
@@ -248,10 +250,10 @@ void dif_kmac_mode_cshake_start(
   if (n_is_empty && s_is_empty) {
     switch (mode) {
       case kDifKmacModeCshakeLen128:
-        dif_kmac_mode_shake_start(kmac, operation_state, kDifKmacModeShakeLen128);
+        dif_kmac_mode_shake_start(kmac, operation_state, kDifKmacModeShakeLen128, msg_endianness);
         return;
       case kDifKmacModeCshakeLen256:
-        dif_kmac_mode_shake_start(kmac, operation_state, kDifKmacModeShakeLen256);
+        dif_kmac_mode_shake_start(kmac, operation_state, kDifKmacModeShakeLen256, msg_endianness);
         return;
       default:
         VPRINTF(ERROR, "dif_kmac_mode_cshake_start: ERROR unsupported mode for empty N and S.\n");
@@ -290,7 +292,9 @@ void dif_kmac_mode_cshake_start(
   operation_state->offset = 0;
 
   // Configure cSHAKE mode with the given strength.
-  uint32_t cfg_reg = (kstrength << KMAC_CFG_SHADOWED_KSTRENGTH_LOW) | (KMAC_CFG_SHADOWED_MODE_VALUE_CSHAKE << KMAC_CFG_SHADOWED_MODE_LOW);
+  uint32_t cfg_reg = (msg_endianness << KMAC_CFG_SHADOWED_MSG_ENDIANNESS_LOW) |
+                     (kstrength << KMAC_CFG_SHADOWED_KSTRENGTH_LOW) |
+                     (KMAC_CFG_SHADOWED_MODE_VALUE_CSHAKE << KMAC_CFG_SHADOWED_MODE_LOW);
   lsu_write_32(kmac + KMAC_CFG_SHADOWED, cfg_reg);
   lsu_write_32(kmac + KMAC_CFG_SHADOWED, cfg_reg);
 
