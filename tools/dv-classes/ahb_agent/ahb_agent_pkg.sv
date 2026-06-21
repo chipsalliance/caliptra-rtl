10febe19a946d0d3fe60e1ac8aa284f3132eb959
Rupert Swarbrick <rswarbrick@lowrisc.org>
2026-06-21 15:44:06 +0100

    [ahb,dv] Implement layering virtual sequence

--- tools/dv-classes/ahb_agent/ahb_agent_pkg.sv
+++ tools/dv-classes/ahb_agent/ahb_agent_pkg.sv
@@ -33,6 +33,9 @@ package ahb_agent_pkg;
   `include "ahb_mgr_driver.svh"
   `include "ahb_mgr_reg_adapter.svh"
 
+  typedef uvm_sequencer#(ahb_reg_op_item) ahb_reg_op_sequencer_t;
+
   `include "seq_lib/ahb_transfer_seq.svh"
   `include "seq_lib/ahb_single_write_seq.svh"
+  `include "seq_lib/ahb_mgr_register_layer_vseq.svh"
 endpackage
