f5b15a5091272a5969a6db3bcdef33d90328e2d5
Rupert Swarbrick <rswarbrick@gmail.com>
2026-06-21 16:48:57 +0100

    [ahb,dv] Give a way to constrain wdata in ahb_transfer_seq
    Extending constraints by writing a subclass is surprisingly tricky!
    Hopefully, this will approach strike a sensible balance between
    convenience and randomising things late enough.
    
    As a "user", the commit adds ahb_single_write_seq, which is designed
    to be easy to randomise when you want to "write 123 to address 456".

--- tools/dv-classes/ahb_agent/seq_lib/ahb_transfer_seq.svh
+++ tools/dv-classes/ahb_agent/seq_lib/ahb_transfer_seq.svh
@@ -37,6 +37,22 @@ class ahb_transfer_seq extends uvm_sequence #(ahb_txn_request_item, uvm_sequence
   // The responses that were seen to the requests in m_requests.
   ahb_txn_response_item m_responses[$];
 
+  // If true then add an extra constraint to an item that gets randomised, forcing its m_wdata field
+  // to equal the sequence's m_fixed_wdata field.
+  protected bit               m_constrain_wdata;
+
+  // If m_constrain_wdata is true when an item is randomised, that item will be constrained to have
+  // an m_wdata field that matches this value.
+  protected rand bit [1023:0] m_fixed_wdata;
+
+  // If true then add an extra constraint to an item that gets randomised, forcing its m_wstrb field
+  // to equal the sequence's m_fixed_wstrb field.
+  protected bit               m_constrain_wstrb;
+
+  // If m_constrain_wstrb is true when an item is randomised, that item will be constrained to have
+  // an m_wstrb field that matches this value.
+  protected rand bit [127:0]  m_fixed_wstrb;
+
   extern function new(string name="");
   extern task body();
 
@@ -46,6 +62,16 @@ class ahb_transfer_seq extends uvm_sequence #(ahb_txn_request_item, uvm_sequence
   // transaction iff there are the same number of responses as requests (so we haven't seen a
   // reset).
   extern function bit has_full_transaction();
+
+  // Randomize the first request item (constrained to match the fields in the sequence)
+  extern virtual protected function void randomize_first_item(ahb_txn_request_item item);
+
+  // Randomise a request item other than the first one, which is passed as the item0 argument.
+  //
+  // The m_addr field will be constrained to match addr.
+  extern virtual protected function void randomize_later_item(ahb_txn_request_item item,
+                                                              ahb_txn_request_item item0,
+                                                              bit [63:0]           addr);
 
   // Consume a response from the driver. If have_seen_reset is true, we have already seen a reset.
   //
@@ -63,6 +89,20 @@ class ahb_transfer_seq extends uvm_sequence #(ahb_txn_request_item, uvm_sequence
 
   // No incrementing burst may cross a 1kb address boundary
   extern constraint increment_1kb_boundary_c;
+
+  // Set m_fixed_wdata to a value that can be represented in a data phase transaction with the given
+  // m_size. (Important if m_constrain_wdata is asserted).
+  //
+  // This is analogous to ahb_txn_request_item::write_size_c, but needs duplicating here because we
+  // randomise m_fixed_wdata at the very start of the sequence.
+  extern constraint fixed_wdata_upper_bound_c;
+
+  // Set m_fixed_wstrb to a value that can be represented in a data phase transaction with the given
+  // m_size. (Important if m_constrain_wstrb is asserted).
+  //
+  // This is analogous to ahb_txn_request_item::write_size_c, but needs duplicating here because we
+  // randomise m_fixed_wstrb at the very start of the sequence.
+  extern constraint fixed_wstrb_upper_bound_c;
 endclass
 
 function ahb_transfer_seq::new(string name="");
@@ -101,23 +141,7 @@ task ahb_transfer_seq::body();
 
   // Randomise the first item that will be sent. This is also used as a template for later transfers
   // (which will use the same m_subordinate_idx, m_burst, m_size, m_write etc.)
-  if (!item0.randomize() with {
-        m_subordinate_idx == local::m_subordinate_idx;
-        m_burst           == local::m_burst;
-        m_size            == local::m_size;
-        m_write           == local::m_write;
-        m_addr            == local::m_addr;
-        m_trans           == TransNonSequential;
-        if (local::m_constrain_wdata && m_trans != TransBusy) {
-          m_wdata == local::m_fixed_wdata;
-        }
-        if (local::m_constrain_wstrb && m_trans != TransBusy) {
-          m_wstrb == local::m_fixed_wstrb;
-        }
-      }) begin
-    `uvm_fatal(get_full_name(), "Failed to randomise item0.")
-  end
-
+  randomize_first_item(item0);
   start_item(item0);
   finish_item(item0);
   m_requests.push_back(item0);
@@ -129,7 +153,6 @@ task ahb_transfer_seq::body();
   while (words_done < m_length && !have_seen_reset) begin
     ahb_txn_request_item item = ahb_txn_request_item::type_id::create("item");
     bit [63:0] item_addr;
-    trans_e    item_trans;
 
     if (m_burst inside {BurstWrap4, BurstWrap8, BurstWrap16}) begin
       // Offsets are measured from the start of the wrap window
@@ -143,27 +166,7 @@ task ahb_transfer_seq::body();
     end
 
     start_item(item);
-
-    // Pick a value for m_trans. This should be either TransSequential or TransBusy (using
-    // m_busy_within_burst_pct as a weighting).
-    item_trans = ($urandom_range(99) < m_busy_within_burst_pct) ? TransBusy : TransSequential;
-
-    if (!item.randomize() with {
-          m_subordinate_idx == local::item0.m_subordinate_idx;
-          m_addr            == local::item_addr;
-          m_burst           == local::item0.m_burst;
-          m_lock            == local::item0.m_lock;
-          m_prot            == local::item0.m_prot;
-          m_size            == local::item0.m_size;
-          m_write           == local::item0.m_write;
-          m_trans dist {
-            TransBusy       :/ local::m_busy_within_burst_pct,
-            TransSequential :/ 100 - local::m_busy_within_burst_pct
-          };
-        }) begin
-      `uvm_fatal(get_full_name(), "Failed to randomise item.")
-    end
-
+    randomize_later_item(item, item0, item_addr);
     finish_item(item);
 
     // Add item to m_requests to show that we've started trying to send this item.
@@ -198,6 +201,51 @@ endtask
 
 function bit ahb_transfer_seq::has_full_transaction();
   return (m_requests.size() == m_responses.size());
+endfunction
+
+function void ahb_transfer_seq::randomize_first_item(ahb_txn_request_item item);
+  if (!item.randomize() with {
+        m_subordinate_idx == local::m_subordinate_idx;
+        m_burst           == local::m_burst;
+        m_size            == local::m_size;
+        m_write           == local::m_write;
+        m_addr            == local::m_addr;
+        m_trans           == TransNonSequential;
+        if (local::m_constrain_wdata && m_trans != TransBusy) {
+          m_wdata == local::m_fixed_wdata;
+        }
+        if (local::m_constrain_wstrb && m_trans != TransBusy) {
+          m_wstrb == local::m_fixed_wstrb;
+        }
+      }) begin
+    `uvm_fatal(get_full_name(), "Failed to randomise first item.")
+  end
+endfunction
+
+function void ahb_transfer_seq::randomize_later_item(ahb_txn_request_item item,
+                                                     ahb_txn_request_item item0,
+                                                     bit [63:0]           addr);
+  if (!item.randomize() with {
+        m_subordinate_idx == local::item0.m_subordinate_idx;
+        m_addr            == local::addr;
+        m_burst           == local::item0.m_burst;
+        m_lock            == local::item0.m_lock;
+        m_prot            == local::item0.m_prot;
+        m_size            == local::item0.m_size;
+        m_write           == local::item0.m_write;
+        m_trans dist {
+          TransBusy       :/ local::m_busy_within_burst_pct,
+          TransSequential :/ 100 - local::m_busy_within_burst_pct
+        };
+        if (local::m_constrain_wdata && m_trans != TransBusy) {
+          m_wdata == local::m_fixed_wdata;
+        }
+        if (local::m_constrain_wstrb && m_trans != TransBusy) {
+          m_wstrb == local::m_fixed_wstrb;
+        }
+      }) begin
+    `uvm_fatal(get_full_name(), "Failed to randomise item.")
+  end
 endfunction
 
 function bit ahb_transfer_seq::consume_response(uvm_sequence_item base_response,
@@ -238,4 +286,12 @@ constraint ahb_transfer_seq::increment_1kb_boundary_c {
   if (m_burst inside {BurstIncr, BurstIncr4, BurstIncr8, BurstIncr16}) {
     ((m_addr + m_length * (1 << m_size) - 1) >> 10) == (m_addr >> 10);
   }
+}
+
+constraint ahb_transfer_seq::fixed_wdata_upper_bound_c {
+  (m_fixed_wdata >> (8 << m_size)) == '0;
+}
+
+constraint ahb_transfer_seq::fixed_wstrb_upper_bound_c {
+  (m_fixed_wstrb >> (1 << m_size)) == '0;
 }
