1259a20590e63cc65714b64eb32c2d4f1c038f9e
Mojtaba Bisheh Niasar <mojtabab@microsoft.com>
2026-09-01 09:20:32 -0700

    Replace proprietary headers with Apache-2.0 in UVMF files

--- src/hmac/uvmf_2022/uvmf_template_output/project_benches/HMAC/tb/sequences/src/HMAC_otf_reset_sequence.svh
+++ src/hmac/uvmf_2022/uvmf_template_output/project_benches/HMAC/tb/sequences/src/HMAC_otf_reset_sequence.svh
@@ -1,14 +1,18 @@
-// ==============================================================================
-// =====            Mentor Graphics UK Ltd                                  =====
-// =====            Rivergate, London Road, Newbury BERKS RG14 2QB          =====
-// ==============================================================================
-// ==============================================================================
-// This unpublished work contains proprietary information.
-// All right reserved. Supplied in confidence and must not be copied, used or disclosed 
-// for any purpose without express written permission.
-// 2019 @ Copyright Mentor Graphics UK Ltd
-// ==============================================================================
-
+//----------------------------------------------------------------------
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//----------------------------------------------------------------------
 
 `ifndef __HMAC_OTF_RESET_SEQUENCE
 `define __HMAC_OTF_RESET_SEQUENCE
