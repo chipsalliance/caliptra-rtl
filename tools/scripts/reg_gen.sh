a50d23113c39b9833c515f8ddfdf67a149d8e19c
Harry Callahan <hcallahan@lowrisc.org>
2026-09-02 11:15:14 +0100

    [ENV] Update register generation scripts to import from rdl/
    Signed-off-by: Harry Callahan <hcallahan@lowrisc.org>

--- tools/scripts/reg_gen.sh
+++ tools/scripts/reg_gen.sh
@@ -16,7 +16,7 @@
 # It is preloaded into the compiler on those blocks via --preload so that its
 # types are visible in the shared root scope during elaboration; consumer RDLs
 # do NOT `include it (see reg_gen.py header comment for rationale).
-KV_DEF=$CALIPTRA_ROOT/src/keyvault/rtl/kv_def.rdl
+KV_DEF=$CALIPTRA_ROOT/src/keyvault/rdl/kv_def.rdl
 
 # Jinja2 template dirs for the DV outputs; reg_gen.py is project-agnostic
 # and takes them explicitly.
@@ -24,18 +24,18 @@ UVM_TPL=$CALIPTRA_ROOT/tools/templates/rdl/uvm
 
 REG_GEN="python3 tools/scripts/reg_gen.py --uvm-template-dir $UVM_TPL"
 
-$REG_GEN $CALIPTRA_ROOT/src/keyvault/rtl/kv_reg.rdl                     --emit-rtl --emit-dv
-$REG_GEN $CALIPTRA_ROOT/src/pcrvault/rtl/pv_reg.rdl                     --emit-rtl --emit-dv
-$REG_GEN $CALIPTRA_ROOT/src/datavault/rtl/dv_reg.rdl                    --emit-rtl --emit-dv
-$REG_GEN --preload $KV_DEF $CALIPTRA_ROOT/src/ecc/rtl/ecc_reg.rdl       --emit-rtl --emit-dv
-$REG_GEN --preload $KV_DEF $CALIPTRA_ROOT/src/sha512/rtl/sha512_reg.rdl --emit-rtl --emit-dv
-$REG_GEN $CALIPTRA_ROOT/src/sha256/rtl/sha256_reg.rdl                   --emit-rtl --emit-dv
-$REG_GEN $CALIPTRA_ROOT/src/sha3/rtl/sha3_reg.rdl                       --emit-rtl --emit-dv
-$REG_GEN $CALIPTRA_ROOT/src/entropy_combiner/rtl/entropy_combiner_reg.rdl --emit-rtl --emit-dv
-$REG_GEN $CALIPTRA_ROOT/src/soc_ifc/rtl/mbox_csr.rdl                    --emit-rtl --emit-dv
-$REG_GEN $CALIPTRA_ROOT/src/soc_ifc/rtl/sha512_acc_csr.rdl              --emit-rtl --emit-dv
-$REG_GEN $CALIPTRA_ROOT/src/soc_ifc/rtl/soc_ifc_reg.rdl                 --emit-rtl --emit-dv
-$REG_GEN --preload $KV_DEF $CALIPTRA_ROOT/src/hmac/rtl/hmac_reg.rdl     --emit-rtl --emit-dv
-$REG_GEN $CALIPTRA_ROOT/src/doe/rtl/doe_reg.rdl                         --emit-rtl --emit-dv
-$REG_GEN $CALIPTRA_ROOT/src/axi/rtl/axi_dma_reg.rdl                     --emit-rtl --emit-dv
-$REG_GEN --preload $KV_DEF $CALIPTRA_ROOT/src/aes/rtl/aes_clp_reg.rdl   --emit-rtl --emit-dv
+$REG_GEN $CALIPTRA_ROOT/src/keyvault/rdl/kv_reg.rdl                     --emit-rtl --emit-dv
+$REG_GEN $CALIPTRA_ROOT/src/pcrvault/rdl/pv_reg.rdl                     --emit-rtl --emit-dv
+$REG_GEN $CALIPTRA_ROOT/src/datavault/rdl/dv_reg.rdl                    --emit-rtl --emit-dv
+$REG_GEN --preload $KV_DEF $CALIPTRA_ROOT/src/ecc/rdl/ecc_reg.rdl       --emit-rtl --emit-dv
+$REG_GEN --preload $KV_DEF $CALIPTRA_ROOT/src/sha512/rdl/sha512_reg.rdl --emit-rtl --emit-dv
+$REG_GEN $CALIPTRA_ROOT/src/sha256/rdl/sha256_reg.rdl                   --emit-rtl --emit-dv
+$REG_GEN $CALIPTRA_ROOT/src/sha3/rdl/sha3_reg.rdl                       --emit-rtl --emit-dv
+$REG_GEN $CALIPTRA_ROOT/src/entropy_combiner/rdl/entropy_combiner_reg.rdl --emit-rtl --emit-dv
+$REG_GEN $CALIPTRA_ROOT/src/soc_ifc/rdl/mbox_csr.rdl                    --emit-rtl --emit-dv
+$REG_GEN $CALIPTRA_ROOT/src/soc_ifc/rdl/sha512_acc_csr.rdl              --emit-rtl --emit-dv
+$REG_GEN $CALIPTRA_ROOT/src/soc_ifc/rdl/soc_ifc_reg.rdl                 --emit-rtl --emit-dv
+$REG_GEN --preload $KV_DEF $CALIPTRA_ROOT/src/hmac/rdl/hmac_reg.rdl     --emit-rtl --emit-dv
+$REG_GEN $CALIPTRA_ROOT/src/doe/rdl/doe_reg.rdl                         --emit-rtl --emit-dv
+$REG_GEN $CALIPTRA_ROOT/src/axi/rdl/axi_dma_reg.rdl                     --emit-rtl --emit-dv
+$REG_GEN --preload $KV_DEF $CALIPTRA_ROOT/src/aes/rdl/aes_clp_reg.rdl   --emit-rtl --emit-dv
